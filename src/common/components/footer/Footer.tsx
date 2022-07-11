60b3bd887e31f1805dddce7bce0b4b223ca3b9d3
Philipp Lehmann <philipp.lehmann@pcode.at>
2022-07-11 13:29:16 +0200

    feat: hide footer if not on landing page

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -1,49 +1,51 @@
 import React from 'react'
-import { Box, IconButton, MenuItem, Stack, Typography } from '@mui/material'
+import {Box, IconButton, MenuItem, Stack, Typography} from '@mui/material'
 import Link from 'next/link'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import styled from '@emotion/styled'
-import { NavbarProps } from '../../types/navbarProps'
-import { Links } from '../../types/links'
+import {useRouter} from "next/router";
+import {NavbarProps} from '../../types/navbarProps'
+import {Links} from '../../types/links'
 
 const links: Links[] = [
-    { url: '', icon: <InstagramIcon /> },
-    { url: '', icon: <FacebookOutlinedIcon /> },
-    { url: '', icon: <GitHubIcon /> },
+    {url: '', icon: <InstagramIcon/>},
+    {url: '', icon: <FacebookOutlinedIcon/>},
+    {url: '', icon: <GitHubIcon/>},
 ]
 
 const FooterContainer = styled.div`
-    width: 100%;
-    position: relative;
-    bottom: 0;
-    left: 0;
-    height: fit-content;
-    line-height: 0;
-    overflow: hidden;
+  width: 100%;
+  position: relative;
+  bottom: 0;
+  left: 0;
+  height: fit-content;
+  line-height: 0;
+  overflow: hidden;
 `
 
 const FooterNavigation = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    min-height: 60px;
-    position: absolute;
-    bottom: 0;
-    left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  min-height: 60px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
 `
 
 export const Footer = (props: NavbarProps) => {
-    return (
+    const router = useRouter();
+    return router.asPath !== '/' ? null : (
         <FooterContainer>
             <Box
                 component="img"
                 sx={{
-                    bottom: { lg: '-5%', xl: '-15%' },
-                    width: { xs: '200%', sm: '150%', md: '100%' },
-                    left: { xs: '-50%', sm: '-25%', md: 0 },
+                    bottom: {lg: '-5%', xl: '-15%'},
+                    width: {xs: '200%', sm: '150%', md: '100%'},
+                    left: {xs: '-50%', sm: '-25%', md: 0},
                     position: 'relative',
                 }}
                 src="/images/footer.svg"
@@ -59,8 +61,8 @@ export const Footer = (props: NavbarProps) => {
                 </Typography>
                 <Stack
                     direction={'row'}
-                    style={{ height: '100%' }}
-                    display={{ xs: 'none', sm: 'flex' }}
+                    style={{height: '100%'}}
+                    display={{xs: 'none', sm: 'flex'}}
                 >
                     {props.routes
                         .filter((route) => route.positions.includes('footer'))
@@ -82,7 +84,7 @@ export const Footer = (props: NavbarProps) => {
                         <IconButton
                             key={index}
                             href={link.url}
-                            sx={{ color: 'primary.dark' }}
+                            sx={{color: 'primary.dark'}}
                         >
                             {link.icon}
                         </IconButton>
