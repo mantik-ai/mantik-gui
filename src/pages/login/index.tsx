fa60de9c1f33cb64b617cac7ff79633acd56bf3d
Philipp Lehmann <philipp.lehmann@pcode.at>
2022-07-11 18:45:53 +0200

    refactor: adjust styling and naming of login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,30 +1,41 @@
 import React from 'react'
-import type {NextPage} from 'next'
-import {Button, Card, CardContent, Divider, IconButton, Link, Stack, TextField, Typography} from "@mui/material";
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import GoogleIcon from '@mui/icons-material/Google';
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import {Links} from "../../common/types/links";
+import type { NextPage } from 'next'
+import {
+    Button,
+    Card,
+    CardContent,
+    Divider,
+    IconButton,
+    Link,
+    Stack,
+    TextField,
+    Typography,
+} from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import GoogleIcon from '@mui/icons-material/Google'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import { Links } from '../../common/types/links'
 
 const signInWith: Links[] = [
-    {url: '', icon: <GoogleIcon/>},
-    {url: '', icon: <FacebookOutlinedIcon/>},
-    {url: '', icon: <GitHubIcon/>},
+    { url: 'https://example.com/4', icon: <GoogleIcon /> },
+    { url: 'https://example.com/5', icon: <FacebookOutlinedIcon /> },
+    { url: 'https://example.com/6', icon: <GitHubIcon /> },
 ]
 
 const Login: NextPage = () => {
     return (
-        <Stack
-            alignItems={'center'}
-            justifyContent={'center'}
-            flexGrow={1}>
-            <Card sx={{
-                pt: 3,
-                width: '100%',
-                maxWidth: '400px',
-                boxShadow: {xs: 0, sm: 3},
-            }}>
+        <Stack alignItems={'center'} justifyContent={'center'} flexGrow={1}>
+            <Card
+                sx={{
+                    p: 3,
+                    pt: 4,
+                    pb: 1,
+                    width: '100%',
+                    maxWidth: '400px',
+                    boxShadow: { xs: 0, sm: 3 },
+                }}
+            >
                 <Divider textAlign="left">
                     <LockOutlinedIcon
                         sx={{
@@ -36,30 +47,65 @@ const Login: NextPage = () => {
                         }}
                     />
                 </Divider>
-                <CardContent sx={{p: 4}}>
+                <CardContent>
                     <Stack direction={'column'} spacing={2}>
-                        <Stack direction='row' justifyContent="space-between" alignItems="center">
-                            <Typography variant="h4" mb={1}>Sign in</Typography>
-                            <Link variant="body2" color="textSecondary" align="right">
-                                Create Account?
+                        <Stack
+                            mb={1}
+                            direction="row"
+                            justifyContent="space-between"
+                            alignItems="center"
+                        >
+                            <Typography variant="h4">Login</Typography>
+                            <Link
+                                href={'/register'}
+                                variant="body2"
+                                color="textSecondary"
+                                align="right"
+                            >
+                                Not yet registered?
                             </Link>
                         </Stack>
-                        <TextField label="email" variant="outlined"/>
-                        <TextField label="password" variant="outlined" type={"password"}/>
-                        <Link variant="body2" color="textSecondary" align="left">
+                        <TextField label="email" variant="outlined" />
+                        <TextField
+                            label="password"
+                            variant="outlined"
+                            type={'password'}
+                        />
+                        <Link
+                            variant="body2"
+                            color="textSecondary"
+                            align="left"
+                        >
                             Forgot password?
                         </Link>
-                        <Button variant="contained" style={{
-                            color: 'white',
-                        }}>
-                            Sign in
+                        <Button
+                            variant="contained"
+                            style={{
+                                color: 'white',
+                            }}
+                        >
+                            login
                         </Button>
-                        <Divider>
-                            <Typography variant="body2" color="textSecondary" align="center">or sign in with</Typography>
+                        <Divider sx={{ py: 1 }}>
+                            <Typography
+                                variant="body2"
+                                color="textSecondary"
+                                align="center"
+                            >
+                                or login with
+                            </Typography>
                         </Divider>
-                        <Stack direction='row' justifyContent='center' spacing={1}>
-                            {signInWith.map(({url, icon}) => (
-                                <IconButton style={{border: "1px solid #80808066"}} key={url} href={url}>
+                        <Stack
+                            direction="row"
+                            justifyContent="center"
+                            spacing={1}
+                        >
+                            {signInWith.map(({ url, icon }) => (
+                                <IconButton
+                                    style={{ border: '1px solid #80808066' }}
+                                    key={url}
+                                    href={url}
+                                >
                                     {icon}
                                 </IconButton>
                             ))}
