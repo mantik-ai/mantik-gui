a3ee2d4987b8bf0ba136a8f87391511755797280
MohammedKevin <kevin.mohammed16@gmail.com>
2022-09-19 14:43:56 +0200

    bug: remove not working setting feature

--- src/pages/projects/details/[id]/settings.tsx
+++ src/pages/projects/details/[id]/settings.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { NextPageWithNestedLayout } from '../../../_app'
 import { DetailsLayout } from '../../../../modules/project_details/layouts/DetailsLayout'
-import ProjectSettings from '../../../../modules/project_details/settings/ProjectSettings'
 
 const SettingsDetails: NextPageWithNestedLayout = () => {
-    return <ProjectSettings />
+    return <></>
 }
 
 SettingsDetails.getNestedLayout = (page) => {
